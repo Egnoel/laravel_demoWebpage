f7cf7ea9b0d0a05b6c1b1d8a6d8dabb6bc6aae11
Egnoel <70013986+Egnoel@users.noreply.github.com>
2025-10-28 10:18:38 +0100

    Forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,31 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function ()  {
-    $jobs = Job::with('employer')->paginate(5);
-    return view('jobs',
+    $jobs = Job::with('employer')->latest()->paginate(3);
+    return view('jobs.index',
         ['jobs'=> $jobs
         ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find((int) $id);
-    return view('job', ['job'=>$job]);
+    return view('jobs.show', ['job'=>$job]);
+});
+
+Route::post('/jobs', function () {
+    Job::create(
+        [
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1,
+        ]
+    );
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
